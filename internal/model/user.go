69e8d825f5b6ba92bdbd96b0e0c7ac3306a87c46
liuman <clearmann@163.com>
2024-07-28 18:48:03 +0800

    first commit

---
+++ internal/model/user.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+    Id        uint   `gorm:"primarykey"`
+    UserId    string `gorm:"unique;not null"`
+    Nickname  string `gorm:"not null"`
+    Password  string `gorm:"not null"`
+    Email     string `gorm:"not null"`
+    CreatedAt time.Time
+    UpdatedAt time.Time
+    DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (u *User) TableName() string {
+    return "users"
+}
